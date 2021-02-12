e38e67fc5444b634ee5ec74a92295d2f12b37947
Kawsar <k4w54r13@gmail.com>
2021-02-13 02:47:40 +0600

    Trying with export-web-to-pdf

--- routes/pdf.js
+++ routes/pdf.js
@@ -1,50 +1,64 @@
 const express = require('express');
 const router = express.Router();
 
-const puppeteer = require('puppeteer');
-const nodemailer = require('nodemailer');
 const fs = require('fs');
+const exportWebToPdf = require('export-web-to-pdf');
+const nodemailer = require('nodemailer');
 
-router.post('/', (req, res) => {
-  console.log('Sendig PDF');
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'pdfmailer2021@gmail.com',
-      pass: 'amikichujanina',
-    },
-  });
-  console.log(req.body.email);
-  var mailOptions = {
-    from: 'pdfmailer2021@gmail.com',
-    to: req.body.email,
-    subject: req.body.name,
-    text: '\nSent with ❤️ from pdfMailer',
-    attachments: [
-      {
-        filename: req.body.name + '.pdf',
-        path: './sample.pdf',
-        contentType: 'application/pdf',
+router.post(
+  '/',
+  (req, res, next) => {
+    exportWebToPdf(req.body.url, {
+      waitForSelectors: ['body'],
+      pdfSettings: {},
+      ignoreHTTPSErrors: true,
+      showBrowserConsole: true,
+    }).then((data) => {
+      fs.writeFileSync('./sample.pdf', data);
+      next();
+    });
+  },
+  (req, res) => {
+    console.log('Sendig PDF');
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'pdfmailer2021@gmail.com',
+        pass: 'amikichujanina',
       },
-    ],
-  };
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-      res.status(500).send("Couldn't send PDF");
-    } else {
-      console.log('Email sent: ' + info.response);
-      fs.unlink('./sample.pdf', (err) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
+    });
+    console.log(req.body.email);
+    var mailOptions = {
+      from: 'pdfmailer2021@gmail.com',
+      to: req.body.email,
+      subject: req.body.name,
+      text: '\nSent with ❤️ from pdfMailer',
+      attachments: [
+        {
+          filename: req.body.name + '.pdf',
+          path: './sample.pdf',
+          contentType: 'application/pdf',
+        },
+      ],
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        res.status(500).send("Couldn't send PDF");
+      } else {
+        console.log('Email sent: ' + info.response);
+        fs.unlink('./sample.pdf', (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
 
-        //file removed
-      });
-      res.contentType('application/pdf');
-      res.send(pdf);
-    }
-  });
-});
+          //file removed
+        });
+        res.contentType('application/pdf');
+        res.send(pdf);
+      }
+    });
+  }
+);
 module.exports = router;
